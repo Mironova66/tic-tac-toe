11a40e7414d55007418e1c5889a6fef6a15e511f
Mironova66 <katyusha.agapova@yandex.ru>
2021-10-20 02:18:20 +0300

    pass first test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React from "react"
 
-export default class extends React.Component {
+interface IProps {}
+
+interface IState {
+  squares: Array<null | string>,
+  step: number,
+  status: 0 | 1 | 2
+}
+
+export default class extends React.Component<IProps, IState> {
+  private winnerLines: Array<Array<number>>;
+
   constructor(props) {
     super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+      step: 0,
+      status: 2
+    };
+
+    this.winnerLines = [
+      // Горизонталь
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // Вертикаль
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // Диагонали
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
   }
 
   render() {
+    const { squares, step, status } = this.state;
+    const user = step % 2 === 0 ? 'x' : 'o';
+
     return (
-      <div>
-        test
+      <div className="game">
+        <div className="score">
+          {status === 0 && <div>Текущий ход: {user}</div>}
+        </div>
       </div>
     );
   }
